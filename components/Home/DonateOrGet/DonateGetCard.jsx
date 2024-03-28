73907e86bd5055da79f8c26616ce1411f5084074
Mishkat433 <mishkat433@gmail.com>
2024-03-28 15:40:38 +0600

    find blood doner insiate

--- components/Home/DonateOrGet/DonateGetCard.jsx
+++ components/Home/DonateOrGet/DonateGetCard.jsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
+import profile from "../../../public/assets/profile.png";
+import { MdAddLocation } from "react-icons/md";
+import { RiPhoneFill } from "react-icons/ri";
 
-const DonateGetCard = () => {
+const DonateGetCard = ({ donner }) => {
     return (
-        <div class="card w- bg-neutral text-neutral-content">
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Cookies!</h2>
-                <p>We are using cookies for no reason.</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Accept</button>
-                    <button class="btn btn-ghost">Deny</button>
-                </div>
+        <div class="card bg-color-bg2 border-1 border-white-text">
+            <div class="card-body -my-3 items-center ">
+                <Image src={profile} alt="profile" height={70} width={70} />
+                <strong class="card-title">{donner?.donnerName}</strong>
+                <p className="text-xl">{donner?.phone}</p>
+                <address className="text-center"> {donner?.address}</address>
+
             </div>
         </div>
     );
