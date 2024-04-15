9da10bd6f14266300c965d9738ec0a42dfddf1d8
Mishkat433 <mishkat433@gmail.com>
2024-04-16 00:57:37 +0600

    change

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page = () => {
     return (
         <div className="h-96 w-full text-center">
-            <h1>Profile page coming soooon</h1>
+            <h1>Profile page coming soon</h1>
 
         </div>
     );
